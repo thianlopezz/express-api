326155c180e59336d3fafce039f482857e4990e5
Thian Lopez Z <thianlopezz@gmail.com>
2020-09-20 14:29:42 -0600

    modi deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
+docker stop api-jm
+docker rm api-jm
+docker rmi thianlopezz/express-api
+docker build --tag thianlopezz/express-api .
+# docker run --name ecodelivery.api -p 3001:3001 -d ecodelivery/ecodelivery.api
 docker run --name api-jm -e PORT=80 -p 3001:80 -d thianlopezz/express-api
